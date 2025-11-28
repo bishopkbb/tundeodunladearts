4d376d5744181f07d6366e03f915fb8ebffc271b
Ajibade Tosin Francis <ajibade_tosin@yahoo.com>
2025-11-28 16:18:35 +0100

    Added favicon

--- frontend/.next/types/validator.ts
+++ frontend/.next/types/validator.ts
@@ -128,6 +128,15 @@ type RouteHandlerConfig<Route extends AppRouteHandlerRoutes = AppRouteHandlerRou
   type __Unused = __Check
 }
 
+// Validate ../../src/app/api/collections/images/route.ts
+{
+  type __IsExpected<Specific extends RouteHandlerConfig<"/api/collections/images">> = Specific
+  const handler = {} as typeof import("../../src/app/api/collections/images/route.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
 // Validate ../../src/app/api/contact/submit/route.ts
 {
   type __IsExpected<Specific extends RouteHandlerConfig<"/api/contact/submit">> = Specific
